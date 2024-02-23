fb5b413adbd3be307e415f4458a6ffa5d6f3f85f
Vlad Cojocaru <vladvcojocaru@gmail.com>
2024-02-23 16:18:29 +0200

    outline

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,4 +1,20 @@
 package emailapp;
 
 public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // Constructor to recive the first name and last name
+
+    // Ask for deprtment
+
+    // Set the mailbox capacity
+
+    // Set the alternate email
+
+    // Change the password
 }
